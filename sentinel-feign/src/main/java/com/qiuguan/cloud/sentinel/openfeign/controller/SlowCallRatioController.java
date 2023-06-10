cb3c4dcfb97717545d7b2901588cb21e61399bbb
qiuguan <18368116334@163.com>
2023-06-10 18:07:19 +0800

    限流模块排除nacos配置和依赖

---
+++ sentinel-feign/src/main/java/com/qiuguan/cloud/sentinel/openfeign/controller/SlowCallRatioController.java
@@ -0,0 +1,25 @@
+package com.qiuguan.cloud.sentinel.openfeign.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author qiuguan
+ * @date 2023/06/10 00:02:07  星期六
+ *
+ * 慢调用控制器
+ */
+@Slf4j
+@RestController
+public class SlowCallController {
+
+    @GetMapping("/slow")
+    public String slowCall() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2);
+        log.info("慢调用正常访问");
+        return "慢调用正常访问";
+    }
+}
--- sentinel-flow/src/main/java/com/qiuguan/cloud/sentinel/flow/SentinelFlowApplication.java
+++ sentinel-flow/src/main/java/com/qiuguan/cloud/sentinel/flow/SentinelFlowApplication.java
@@ -6,8 +6,17 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author qiuguan
- * @date 2023/05/16 14:18:14  星期二
+ * @date 2023/06/10 00:01:25  星期六
+ *
+ * 排除nacos, 因为我的主pom中导入了nacos依赖，所以sentinel-feign直接就继承了，
+ * 但是我这里测试是sentinel,和nacos关系不大（持久化会用到），所以我这里把它排除掉,
+ * 还有一种方式，请看sentinel-flow的pom.xml
  */
+//@SpringBootApplication(exclude = {
+//        NacosDiscoveryAutoConfiguration.class,
+//        NacosDiscoveryClientConfiguration.class,
+//        NacosServiceRegistryAutoConfiguration.class
+//})
 //@EnableDiscoveryClient
 @SpringBootApplication
 public class SentinelFlowApplication {
